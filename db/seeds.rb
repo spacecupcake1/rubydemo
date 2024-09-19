d648878fe9b478c99c6e74bcb3a6d403c884e2cd
spacecupcake1 <rigani.jegatheeswaran@bbzbl-it.ch>
2024-09-19 10:58:56 +0200

    created Products in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create(name: "Product 1", quantity: 10)
+Product.create(name: "Product 2", quantity: 20)
+Product.create(name: "Product 3", quantity: 30)
+Product.create(name: "Product 4", quantity: 40)
+Product.create(name: "Product 5", quantity: 50)
